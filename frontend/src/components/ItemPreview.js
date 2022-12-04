3b3a3c2ef4fd953e9dcd183062f8ffb10ef3d055
Belunatic <abelluvyou@yahoo.com>
2022-12-04 10:57:11 -0800

    Fixing the broken image on ItemPreview component

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-
-//placeholder.png
 import placeholder from '../imgs/placeholder.png'
 
 const mapDispatchToProps = (dispatch) => ({
